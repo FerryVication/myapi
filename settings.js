9ebf31eacf0dd29c59e57bc60e4e1c540c34f773
FerryVication <feridyanpratama@gmail.com>
2024-07-16 21:08:44 +0700

    Hey

---
+++ settings.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+global.creator = 'FeriPratama'// yourname
+global.MONGO_DB_URI = 'mongodb+srv://dbapi.wpnw1fu.mongodb.net/" --apiVersion 1 --username db_api --password Cbp4gSjoN5NzGrDC' //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "verbotx@gmail.com" //email
+global.email_password = "bqghpykibqzlhvlv" //application password email
+global.limitCount = 1000
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
